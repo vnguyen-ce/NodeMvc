f703f4e3b44bec00739ceb980a75ac55aa631d2f
Van T Nguyen <van.doing.net@gmail.com>
2011-08-27 17:46:05 +1000

    Move code from app.js to MvcApplication.js

---
+++ MvcApplication.js
@@ -0,0 +1,31 @@
+var System =  require('./System'); 
+System.extend(require('./System/Web')); 
+System.extend(require('./System/Web/Routing'));
+
+var router = new System.Web.Routing.Router();
+
+var MvcApplication = {	
+	HandleRequest: function(request, response) {
+		var route = router.ResolveRequest(request);
+		if (route == null) {
+			// process by a FileSystemHandler;
+			console.log('Will be implemented by a FileSystemHandler');
+		}
+		var handler = route.RouteHandler;
+		if (handler == null){
+			handler = new System.Web.SimpleHttpHandler(); 
+		}
+		
+		handler.ProcessRequest(new System.Web.HttpContext(request, response));
+	},
+	
+	RegisterRoutes: function(routeCollection){
+		routeCollection.Ignore('/favicon.ico');
+		routeCollection.Add('Default', new System.Web.Routing.Route('/controller/action/id'));
+		console.log(routeCollection);
+	}	
+};
+
+MvcApplication.RegisterRoutes(System.Web.Routing.RouteTable.Routes);
+
+module.exports = MvcApplication;
--- app.js
+++ app.js
@@ -1,33 +1,12 @@
 var http = require('http');
 var util = require('util');
-// Include all namespaces in System folder
-var System =  require('./System'); 
-System.extend(require('./System/Web')); 
-System.extend(require('./System/Web/Routing'));
 
-// Register routes to route table
-var RouteTable = System.Web.Routing.RouteTable;
-RouteTable.Routes.Ignore('/favicon.ico');
-RouteTable.Routes.Add('Default', new System.Web.Routing.Route('/controller/action/id'));
-
-
-//console.log(RouteTable.Routes);
-var router = new System.Web.Routing.Router();
+var MvcApplication = require('./MvcApplication.js');
 
 // APP	
 http.createServer(function (req, res) {
-	var route = router.ResolveRequest(req);
-	if (route == null) {
-		// process by a FileSystemHandler;
-		console.log('Will be implemented by a FileSystemHandler');
-	}
-	var handler = route.RouteHandler;
-	if (handler == null){
-		handler = new System.Web.SimpleHttpHandler(); 
-	}
-	
-	handler.ProcessRequest(new System.Web.HttpContext(req, res));
-	//console.log(util.inspect(process.memoryUsage()));
+	MvcApplication.HandleRequest(req, res);	
+	console.log(util.inspect(process.memoryUsage()));
 }).listen(1337, "127.0.0.1");
 
 console.log('Server running at http://127.0.0.1:1337/');
